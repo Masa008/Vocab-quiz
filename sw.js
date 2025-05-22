c6cd6dbeae96719b13ce65b2a10041ac71ffffee
Masa008 <masahiro.y0912@gmail.com>
2025-05-23 00:10:42 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,10 @@
-self.addEventListener('fetch',()=>{});
+
+const CACHE='vocab-v3';
+const FILES=['/','/index.html','/style.css','/app.js','/sheets.json'];
+
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
